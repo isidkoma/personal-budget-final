d6dad357c68e069457da72b2372fdef5a38b497a
isidkoma <143963038+isidkoma@users.noreply.github.com>
2023-12-15 01:01:56 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -206,6 +206,6 @@ function validateToken(token, res, callback) {
   }
 }
 
-app.listen(PORT,'155.138.193.44', () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
